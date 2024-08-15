5405d3a30c8f7eda46244f01b05eef5772a9c348
root <mintesinotteferamt@gmail.com>
2024-08-15 11:24:39 -0700

    task1

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-add = __import__('0-add').add
+''' Description: takes a string str1 and a string str2 as arguments
+    and returns a concatenated string
+    arguments: str1: str
+               str2: str
+'''
 
-print(add(1.11, 2.22) == 1.11 + 2.22)
-print(add.__annotations__)
+
+def concat(str1: str, str2: str) -> str:
+    ''' Return concatenation of str1 and str2. '''
+    return str1 + str2
